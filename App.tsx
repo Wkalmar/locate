9e1ba25cc2ec3576af5f795d3478353d77ff98ec
Bohdan Stupak <stupakbogdan@ukr.net>
2022-05-11 19:05:10 +0300

    custom logo

--- App.tsx
+++ App.tsx
@@ -53,7 +53,7 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
   }, [])
 
   return (
